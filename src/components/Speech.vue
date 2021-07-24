9b0213564d84dabe32b05240ea0405cca72038eb
Raül San Clemente <raulsancal@gmail.com>
2021-07-24 10:04:43 +0200

    added speech

--- src/components/Speech.vue
+++ src/components/Speech.vue
@@ -1,6 +1,195 @@
 <template>
   <h1>El Discurs</h1>
-  <div v-html="speech.body" class="speech-body"></div>
+  <div class="speech-body">
+    <p>
+      Matut, canut, mathews, peix, mateix&#8230; m&uacute;ltiples s&oacute;n els
+      noms que t&rsquo;acompanyen, de la mateixa manera que tu batejes a coses i
+      persones per igual (alletges).
+    </p>
+
+    <p>Comencem repassant la teva infantesa&#8230;</p>
+
+    <p>
+      Potser la gent no ho sap, per&ograve; ja erets un kinki de ben petit.
+      T&rsquo;ho va ensenyar la universidad de la calle. Concretament, el
+      p&iacute;o doce. Grans esplais vas fer all&agrave;. Vas aprendre els
+      millors insults, a conquerir places, a tirar pedres amb tiraxines, vas
+      con&egrave;ixer el Ro&ntilde;as i el primer amor de Huka, segurament ja ni
+      s&rsquo;enrecorda, era la gran de 5 o 6 germans, els seus pares eren
+      cosins com a m&iacute;nim i vivien al portal de davant de casa.
+    </p>
+
+    <p>
+      No tot era coses xungues al barri, tamb&eacute; vas rescatar un gatet i
+      vas embaucar als brasilers de la papereria perqu&egrave; l&rsquo;operessin
+      i se'l quedessin.
+    </p>
+
+    <p>
+      Tamb&eacute; &eacute;s cert que ja des de petit erets un emprenedor. Vas
+      inventar els Tazos 3d. Com? Doncs posant tazos normals a una
+      l&agrave;mpara i deixant que es desfessin. D&rsquo;alguna forma Matutano
+      ho va descobrir i els va treure un temps despr&eacute;s.
+    </p>
+
+    <p>
+      Hem de dir que sempre t&rsquo;han agradat els animals de tot
+      tipus...qualsevol diria que hauries d&rsquo;haver estudiat per ser
+      veterinari, cuidador en un Zoo, o encara m&eacute;s, protagonista
+      intr&egrave;pid del National Geographic. Has sigut col&middot;leccionista
+      d&rsquo;animals des de ben petit. En el teu palmar&egrave;s hi figuren: un
+      conill, 5 generacions de h&agrave;msters, un criadero de cacatues (esta
+      cacat&uacute;a es kara? lo siento aqu&iacute; no hablamos vasco), peixos
+      varis i de tots colors, un parell de gossos, unes tortugues, 5
+      salamandres, 2 ping&uuml;ins i tres velociraptors&#8230; exagerem? Segur
+      que s&iacute;, per&ograve; t&uacute; ets el mestre de posar m&eacute;s pa
+      que formatge a les hist&ograve;ries. La Carla no sap la sort que t&eacute;
+      de que estiguis viciat a l'ordinador jugant al Planet Zoo, mentre sigui
+      aix&iacute;, nom&eacute;s haur&agrave;s colat a l&rsquo;&Agrave;sia.
+    </p>
+
+    <p>I va venir l&rsquo;adolesc&egrave;ncia al Voramar...</p>
+
+    <p>
+      Van anar passant els anys i el teu pare va decidir patrocinar en el nostre
+      club (3x3 nou visca el poblenou! Crits de tots!!), des de llavors tots
+      teniem la nostra bossa Choleck, la tovallola de Chufi, i un conjunt de
+      samarretes de l&rsquo;estil que solia portar en Harna quan erem uns crios
+      (university of Southampton, I love NY). No hi ha hagut mai cap
+      inversi&oacute; tan gran en la hist&ograve;ria de l&rsquo;Handbol
+      Poblenou. Pensa que en aquells temps no hi havia cryptos on llen&ccedil;ar
+      els diners, aix&iacute; que qu&egrave; millor que regalar 4 coses a
+      l&rsquo;equip del teu fill. Vas tenir un paper destacat en l&rsquo;equip.
+      Estar en el set titular ens donava confian&ccedil;a. Com a lateral dret, i
+      algun que altre dia, pivot, erets una jugador polivalent per la teva
+      capacitat de jugar amb les dues mans, i sempre mirant en direcci&oacute; a
+      porteria, creant multitud de penals i faltes quan feies que els defensor
+      t&rsquo;agafessin del coll. Alguna que altra vegada, posaves de la teva
+      part, per&ograve; els que hem jugat a handbol sabem que calia fer-ho per
+      buscar els 2 minuts del defensor contrari.
+    </p>
+
+    <p>
+      Son innombrables els records que tenim a trav&eacute;s del territori
+      catal&agrave;, des del frontons de Solius, les sortides per Platja
+      d&rsquo;Aro fent stop a fer-nos fotos amb les ovelles, les BBQ a Sant Pol
+      de Mar (St Pol quina hora &eacute;s??), les festes a casa el Carlitos
+      (pisito i Calafell), passant pels Basurilles de river, quan no teniem un
+      duram (i Lleida), i les escapades cap a Conil de la Frontera i Ibiza...
+    </p>
+
+    <p>
+      Si hi ha un lloc que va determinar els estius, aquest va ser
+      Torredembarra, un dest&iacute; privilegiat. Dies llargs a la platja,
+      campionats de futbol platja, i nits interminables al Nose, entre desenes
+      d&rsquo;amics que anaven sortint de sota les pedres. Van ser anys de
+      bogeria i felicitat per tots nosaltres.
+    </p>
+
+    <p>
+      Tamb&eacute; recordem aquells caps de setmana, aprofitant que els teus
+      pares marxaven a Torre, amb maratons de Volt&iacute;metre, sempre amb
+      patates fregides congelades (&ldquo;atoves&rdquo; per dins cruixents per
+      fora) per fer les cl&agrave;ssiques &ldquo;bravinhes&rdquo;, les pizzes
+      amb oli picant (amb molta salsa picant..), la carbonyesa... els
+      llan&ccedil;aments a la mini canasta a la teva habitaci&oacute; o els xuts
+      sa porteria al teu menjador.d Otqsga
+    </p>
+
+    <p>
+      El 30 aniversari: Quina millor manera de poder-lo celebra, en quin millor
+      escenari que NEW YORK a prendre pel cul de casa, en una ciutat que ens
+      v&agrave;rem fer els reis. La teva rialla i alegria despr&egrave;n energia
+      positiva i ganes de tirar endavant, bon amic de cor, amic com els que no
+      hi han, aquest ets tu: Aleix( Mazius)
+    </p>
+
+    <p>
+      Parlem de festa, sempre has estat l&rsquo;embaucador n&ordm; 1 per liarnos
+      a sortir a un lloc o un altre, encara que fos a la merda del
+      Merl&iacute;n. Els nostres santuaris s&oacute;n m&iacute;tics,
+      comen&ccedil;ant per Marina quan ten&iacute;em nom&eacute;s 16 anys, els
+      primers xupitos i cubates els vam fer all&agrave; (No lo s&eacute;, Como
+      tu, BB+, i un llarg etc&egrave;tera), lu bo &eacute;s que pillavem la
+      papiglia i anavem caminant cap a casa.. Despr&eacute;s vam augmentar el
+      nivell, descobrint el SHE i MALALTS de Festa, amb incontables &ldquo;hasta
+      abajos&rdquo; i &ldquo;perreando&rdquo; fins la matinada. Sort en
+      ten&iacute;em d&rsquo;aquella camara digital que portaves cada nit com si
+      fos una extremitat m&eacute;s del teu cos, la quantitat de fotos i moments
+      gravats &eacute;s incontable, i ens servia al dia seg&uuml;ent per refer
+      les nits &ldquo;de locura&rdquo;. Recordem els xupitos de Ballantines
+      nom&eacute;s entrar, que ja et deixaven la cara de pomes agres per una
+      bona estona, la &ldquo;bala roja&rdquo; que m&eacute;s d&rsquo;un ensurt
+      ens ha donat, per&ograve; sempre fidel portant-nos al nostre dest&iacute;,
+      i mai oblidarem les c&oacute;reos del titus o les papilles a la milonga.
+    </p>
+
+    <p>
+      Sempre has sigut l&rsquo;amo i senyor del reggaeton, pioner i descubridor
+      de &ldquo;temazos&rdquo; abans que es posessin de moda i seguint les
+      &uacute;ltimes novetats com a bon DJ. M&eacute;s d&rsquo;un podria pensar
+      que ets familiar directe de Daddy Yankee o Don Omar...
+    </p>
+
+    <p>
+      Grans tardes de divendres fent &ldquo;ramblinha&rdquo; menjant pipes i
+      xuxes i xerrant fins les tantes. Anar al duram a fer un platet amb extra
+      de salsa i dirli sempre al cambrer &ldquo;la ultima vez nos pusiste muy
+      poco&rdquo; per a que portes un plat a reventar Xutar a les faroles de
+      montju&iuml;c va ser un pla alternatiu quan no sabiem que fer.
+    </p>
+
+    <p></p>
+
+    <p>
+      I per seguir la l&iacute;nea de fiestero, mai oblidarem que en una de les
+      festes, concretament a la disco la Fira, rondant el 2016, entre chupitos
+      de Jagger vam decidir anar a viure junts! La Camarrusa, Harna Barna i la
+      Minain! I a partir d'alla ens vam convertir en una petita fam&iacute;lia
+      on hi havia festes finde tras finde, i els soparets, i els &quot;jo faig
+      l'hummus&quot;, els formatges i vins, les rises a les nits, les xerrades
+      als matins, el senyor harna i els &quot;hem de reciclar&quot; la xexi i la
+      seva capacitat de dormir molt i la minain amb els portazos de bon
+      mat&iacute;. Van ser un anys &uacute;nics i agra&iuml;m compartir-los amb
+      t&uacute; perqu&egrave; ens has ensenyat a riure les penes i a saber
+      afrontar les coses des del punt de vista m&eacute;s positiu, perqu&egrave;
+      tu ets aix&iacute;, bromista, optimista i una persona molt sentimental i
+      amb un cor enorme! Despr&eacute;s d'aquests anys al pis compis o
+      shocolatong o els mils de mots que vulguis dir, vas con&egrave;ixer a la
+      que avui &eacute;s la teva dona, all&agrave; vas deixar el pis i vas
+      comen&ccedil;ar a viure amb la persona que et far&agrave; feli&ccedil; la
+      resta dels teus dies, la Carla.
+    </p>
+
+    <p>
+      Sempre vas tenir clar el teu futur, el m&oacute;n del motor. Son poques
+      les persones que tenen clar all&ograve; que volen fer i menys les que ho
+      aconsegueixen. Estem segurs que no es facil passar tant de temps lluny de
+      casa, per&ograve; &eacute;s un privilegi escoltar-te parlar de la teva
+      feina, dels teus pilots, i m&eacute;s encara, de veuret per la
+      televisi&oacute; aquells que seguim les motos i celebrem les
+      vict&ograve;ries dels teus pilots. Tamb&eacute; estem molt agra&iuml;ts
+      que la teva experiencia en l'&agrave;mbit de la competici&oacute;
+      l&rsquo;apliquis als motoristes amics i coneguts sota el nom d&rsquo;AP
+      motorcycles, sempre disposat a donar un cop d&rsquo;ull o asesora en la
+      compra de motos.
+    </p>
+
+    <p>
+      Com pots veure ens has marcat a les nostres vides des de que erets ben
+      petit, i ets una persona que no deixa indiferent a ning&uacute;. Sempre
+      has estat un tio decidit en all&ograve; que has fet, a vegades amb opcions
+      de vestimenta i pentinats atrevits. Sabem que aquest &uacute;ltim any i
+      mig no ha estat f&agrave;cil i ens has mostrat la teva maduresa i saber
+      estar en nombroses ocasions. Per aix&ograve; et volem recordar que sempre
+      estarem al teu costat, com ho hem fet fins ara, i desitjar-te el millor
+      dels futurs al costat de la teva futura dona Carla.
+    </p>
+
+    <p>Atentament els teus amics.</p>
+
+    <p>Crit &uacute;ltim per acabar: &ldquo;Visca els nuvis!!!&rdquo;</p>
+  </div>
 </template>
 
 <script>
